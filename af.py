5f4050e0d8f33ea2262763f665d81b62806c134e
renemichelnunes <rene.michel@gmail.com>
2020-10-06 00:31:02 -0300

    Update af.py

--- af.py
+++ af.py
@@ -216,7 +216,10 @@ class AF():
                     if len(temp) > 1 and temp not in estados:
                         #print('novo estado! {}'.format(temp))
                         estados.append(temp) # acrescente na lista de estados
-                        tabela.append([[], [], [], []]) # adicione uma linha na tabela de transições
+                        linha = []
+                        for i in self.alfabeto:
+                            linha.append([])
+                        tabela.append(linha) # adicione uma linha na tabela de transições
             
     
         #print('lista de estados atualizada')
