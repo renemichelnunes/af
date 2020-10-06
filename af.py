708d92fc0e0901a7f501ebfbb484d8848001227b
renemichelnunes <rene.michel@gmail.com>
2020-10-06 01:58:07 -0300

    Update af.py

--- af.py
+++ af.py
@@ -178,7 +178,7 @@ class AF():
                 estados.append([i.de.nome])
             if [i.para.nome] not in estados:
                 estados.append([i.para.nome])
-        estados.sort()
+        estados.sort(key=lambda x: int(x[0][1:]))
         #print(estados)
 
         #print('criando tabela vazia de transições')
